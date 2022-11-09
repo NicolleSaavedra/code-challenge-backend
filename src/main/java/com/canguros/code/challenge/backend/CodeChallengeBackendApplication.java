751018bc2c988564aa21817d2661944860c5d30f
Nicolle Vanessa Saavedra Daza <47528156+NicolleSaavedra@users.noreply.github.com>
2022-11-09 01:07:54 -0500

    Initial commit

---
+++ src/main/java/com/canguros/code/challenge/backend/CodeChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.canguros.code.challenge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canguros/code/challenge/backend/CodeChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canguros.code.challenge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
